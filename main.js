71d4c97fbdd90953b29fb18fa213fbbfc5877b29
ddori <gotsla914@naver.com>
2022-03-10 00:02:40 +0900

    hide and show left nav script

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
 const srchBar = document.querySelector('.srchBar');
 const srchIcon = document.querySelector('.srchIcon');
 
-function openSrchBar(){
-  if(srchBar.style.display == 'none'){
+function openSrchBar() {
+  if (srchBar.style.display == 'none') {
     srchBar.style.display = 'block'
   } else {
     srchBar.style.display = 'none'
@@ -16,3 +16,23 @@ function openSrchBar(){
 }
 
 srchIcon.addEventListener('click', openSrchBar);
+
+// 왼쪽 네비
+
+// ul 선택 (큰 메뉴)
+// 하위 li 모두 display block 됨
+// 재 클릭시 display none
+
+const openLi = document.querySelector('.openLi'); //클릭li
+const openChild = document.querySelector('.menuSpacing');
+
+
+function Openleft() {
+  if (openChild.style.display == 'none'){
+    openChild.style.display = 'block';
+  } else {
+    openChild.style.display = 'none';
+  }
+}
+
+openLi.addEventListener('click', Openleft);
